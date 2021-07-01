dea6f9d18e6ee07b3eb6b98b983b32be6f3b4839
Leo Preciado <leoipn@gmail.com>
2021-07-01 02:18:23 -0500

    Plotly - Horizontal Bar chart done

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,55 +1,45 @@
 function unpack(rows, index) {
     return rows.map(function(row) {
-        console.log(row.id);
+        //console.log(row.id);
         return row[index];
     });
   }
-  
-d3.json("samples.json").then((data) => {
-    //console.log(data.samples);
-    var samples = data.samples;
-    var ids = samples.map(sample => sample.id);
-    
+
+function fillDropDown(samples){
     d3.select("#selDataset")
     .selectAll("option")
     .data(samples)
     .enter()
     .append("option")
     .html(x => `${x.id}`);
-    //console.log(unpack(data.samples,1));
+}
 
-    var sample_values = data.samples.sample_values;
-    var otu_ids = data.samples.otu_ids;
+d3.json("samples.json").then((data) => {
+
+    var samples = data.samples;
+    var firsElement = samples[0].id;
+
+    var firstSample = samples.filter(x => x.id === firsElement)[0];
+    var sample_values = firstSample.sample_values.slice(0,10);
+    //console.log(sample_values);
+    var otu_ids = firstSample.otu_ids.map(x => 'OTU '+x).slice(0,10);
+    //console.log(otu_ids);
+    
+    var otu_labels = firstSample.otu_labels.slice(0,10);
+    //console.log(otu_labels);
+    
+    fillDropDown(samples);
+
     
     var data = [{
         type: 'bar',
-        x: sample_values,
-        y: otu_ids,
-        orientation: 'h'
+        x: sample_values.reverse(),
+        y: otu_ids.reverse(),
+        orientation: 'h',
+        text:otu_labels.reverse()
       }];
     
       Plotly.newPlot('bar', data);
 
-    // //  Create the Traces
-    // var trace1 = {
-    //   x: data.organ,
-    //   y: data.survival.map(val => Math.sqrt(val)),
-    //   type: "box",
-    //   name: "Cancer Survival",
-    //   boxpoints: "all"
-    // };
-  
-    // // Create the data array for the plot
-    // var data = [trace1];
-  
-    // // Define the plot layout
-    // var layout = {
-    //   title: "Square Root of Cancer Survival by Organ",
-    //   xaxis: { title: "Organ" },
-    //   yaxis: { title: "Square Root of Survival" }
-    // };
-  
-    // // Plot the chart to a div tag with id "plot"
-    // Plotly.newPlot("plot", data, layout);
   });
  
