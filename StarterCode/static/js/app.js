54e6641e60ec7a49f1c041a95481889eb64f4bf4
Leo Preciado <leoipn@gmail.com>
2021-07-01 23:57:32 -0500

    Plotly - Bubble chart updated

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -5,34 +5,53 @@ function unpack(rows, index) {
     });
   }
 
-function fillDropDown(samples){
+function fillDropDown(names){
     d3.select("#selDataset")
     .selectAll("option")
-    .data(samples)
+    .data(names)
     .enter()
     .append("option")
-    .html(x => `${x.id}`);
+    .html(x => `${x}`);
 }
 function showDashboard(subjectId){
     d3.json("samples.json").then((data) => {
-
+        
+        var names = data.names;
+        var metadata = data.metadata;
         var samples = data.samples;
+        
         //var firstElement = samples[0].id;
         var oneSample = samples.filter(element => element.id === subjectId)[0];
+        var oneMetadata = metadata.filter(element => element.id === parseInt(subjectId))[0];
+        //console.log(names);        
 
         var sample_values = oneSample.sample_values;
         var sliced_sample_values = sample_values.slice(0,10);
 
         var otu_ids = oneSample.otu_ids;
+        console.log(otu_ids);
         var sliced_otu_ids= otu_ids.map(x => 'OTU '+x).slice(0,10);
         
         var otu_labels = oneSample.otu_labels;
         var sliced_otu_labels = otu_labels.slice(0,10);
         
-        fillDropDown(samples);
+        fillDropDown(names);
+        fillMetaData(oneMetadata);
         barChart(sliced_sample_values, sliced_otu_ids, sliced_otu_labels);
         bubbleChart(otu_ids, sample_values, otu_labels);
     });
+}
+
+function fillMetaData(oneMetadata){
+    var div = d3.select("#sample-metadata")
+    div.html("");
+    div.append("p").text(`ID: ${oneMetadata.id}`);
+    div.append("p").text(`ETHNICITY: ${oneMetadata.ethnicity}`);
+    div.append("p").text(`GENDER: ${oneMetadata.gender}`);
+    div.append("p").text(`AGE: ${oneMetadata.age}`);
+    div.append("p").text(`LOCATION: ${oneMetadata.location}`);
+    div.append("p").text(`BBTYPE: ${oneMetadata.bbtype}`);
+    div.append("p").text(`WFREQ: ${oneMetadata.wfreq}`);
 }
 
 function barChart(sample_values, otu_ids, otu_labels){
@@ -55,8 +74,9 @@ function bubbleChart(otu_ids, sample_values, otu_labels){
         text:otu_labels,
         mode: 'markers',
         marker: {
-          color: otu_ids,
-          opacity: [1, 0.8, 0.6, 0.4],
+          color: [41,342],//otu_ids,
+          sizeref: 1.2,
+          //opacity: [1, 0.8, 0.6, 0.4],
           size: sample_values
         }
       };
@@ -64,13 +84,15 @@ function bubbleChart(otu_ids, sample_values, otu_labels){
       var data = [trace1];
       
       var layout = {
-        title: 'Marker Size and Color',
         showlegend: false,
-        height: 780,
-        width: 1024
+        xaxis: {
+            title: {
+              text: 'OTU ID',
+            }
+          }
       };
       
-      Plotly.newPlot('bubble', data, layout);
+      Plotly.newPlot('bubble', data, layout, {scrollZoom: true});
 }
 
 function optionChanged(x){
